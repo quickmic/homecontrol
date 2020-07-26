a08456c6e30b9558e3cfdf437308ad3ad5e106a4
root <root@server.rms>
2020-07-26 18:44:13 +0200

    update sensors for adaptec raid controller and smartmon

--- plugins/sensors.py
+++ plugins/sensors.py
@@ -38,14 +38,21 @@ class Controller(multiprocessing.Process):
 		IDExternal = self.Settings["sensorsid"] + "/"
 		self.ComQueue[MQTT].put([MQTT_PUBLISH, IDExternal + "interface", self.Settings[SETTINGS_IPTOPIC]])
 		WifiDevice = []
+		SmartDevice = []
+		SmartLabels = []
 		Label = []
 		InputFile = []
 		PrevData = []
 		PrevRaspiGPU = ""
 		PrevWifi = []
 		PrevDMU = ""
+		PrevAdaptec = []
+		PrevAdaptecHDD = []
+		PrevSmart = []
 		Raspi = False
 		DMU = False
+		Adaptec = False
+		Smart = False
 
 		if os.path.isdir("/sys/class/hwmon/"):
 			arr = os.listdir("/sys/class/hwmon/")
@@ -77,6 +84,50 @@ class Controller(multiprocessing.Process):
 			Raspi = True
 			self.Settings[SETTINGS_LOGGER].debug("Sensors Raspberry Pi detected")
 
+		if os.path.isfile("/sbin/blkid"):
+			if os.path.isfile("/usr/sbin/smartctl"):
+				arr = os.listdir("/dev/")
+
+				for i in range(0, len(arr)):
+					if arr[i][:2] == "sd" and len(arr[i]) != 3:
+						Temp = os.popen("/sbin/blkid /dev/" + arr[i]).read()
+						Pos = Temp.find("UUID=")
+						Temp = Temp[Pos + 6:]
+						Pos = Temp.find('"')
+						Temp = Temp[:Pos].strip()
+						SmartDevice.append(Temp)
+						SmartLabels.append([])
+						PrevSmart.append([])
+						UUID = Temp
+						Temp = os.popen("/usr/sbin/smartctl -A /dev/disk/by-uuid/" + UUID).read()
+						Lines = Temp.split("\n")
+
+						for j in range(0, len(Lines)):
+							Pos = Lines[j].find("Temperature")
+
+							if Pos != -1:
+								Pos = Lines[j].find(" ")
+								Lines[j] = Lines[j][Pos:].strip()
+								Pos = Lines[j].find(" ")
+								HDDLabel = Lines[j][:Pos].strip()
+								SmartLabels[len(SmartLabels) - 1].append(HDDLabel)
+								PrevSmart[len(SmartLabels) - 1].append("-1")
+								Smart = True
+
+		if os.path.isfile("/usr/StorMan/arcconf"):
+			Temp = os.popen("/usr/StorMan/arcconf LIST").read()
+			Pos = Temp.find("Controllers found")
+			Temp = Temp[Pos + 18:]
+			Pos = Temp.find("\n")
+			Temp = Temp[:Pos].strip()
+
+			for i in range(0, int(Temp)):
+				PrevAdaptecHDD.append(["-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"])
+
+				PrevAdaptec.append("-1")
+				Adaptec = True
+				self.Settings[SETTINGS_LOGGER].debug("Adaptec Raid Controller " + str(i) + "detected")
+
 		if os.path.isfile("/proc/dmu/temperature"):
 			DMU = True
 
@@ -90,6 +141,21 @@ class Controller(multiprocessing.Process):
 
 		while True:
 			time.sleep(Delay)
+
+			if Smart:
+				for i in range(0, len(SmartDevice)):
+					for j in range(0, len(SmartLabels[i])):
+						Temp = os.popen("/usr/sbin/smartctl -A /dev/disk/by-uuid/" + SmartDevice[i]).read()
+						Pos = Temp.find(SmartLabels[i][j])
+						Temp = Temp[Pos:]
+						Pos = Temp.find("\n")
+						Temp = Temp[:Pos]
+						Pos = Temp.rfind(" ")
+						Temp = Temp[Pos:].strip()
+
+						if PrevSmart[i][j] != Temp:
+							PrevSmart[i][j] = Temp
+							self.ComQueue[MQTT].put([MQTT_PUBLISH, IDExternal + "temperature/hdd-uuid-" + SmartDevice[i] + "/" + SmartLabels[i][j], Temp])
 
 			if DMU:
 				File = open("/proc/dmu/temperature", "r", encoding="ISO-8859-1")
@@ -114,6 +180,36 @@ class Controller(multiprocessing.Process):
 				if PrevRaspi != data:
 					PrevRaspi = data
 					self.ComQueue[MQTT].put([MQTT_PUBLISH, IDExternal + "GPU", Temp])
+
+			if Adaptec:
+				for i in range(0, len(PrevAdaptec)):
+					time.sleep(1)
+					Temp = os.popen("/usr/StorMan/arcconf getconfig " + str(i + 1) + " AD").read()
+					Pos = Temp.find("Temperature")
+					Temp = Temp[Pos + 11:]
+					Pos = Temp.find("\n")
+					Temp = Temp[:Pos]
+					Temp = Temp.replace(":", "").strip()
+					Pos = Temp.find("C")
+					Temp = Temp[:Pos].strip()
+
+					if PrevAdaptec[i] != Temp:
+						PrevAdaptec[i] = Temp
+						self.ComQueue[MQTT].put([MQTT_PUBLISH, IDExternal + "temperature/adaptec" + str(i), Temp])
+
+					time.sleep(1)
+					Temp = os.popen("/usr/StorMan/arcconf GETSMARTSTATS " + str(i + 1)).read()
+					TempHDD = Temp.split("Temperature")
+
+					for j in range(1, len(TempHDD)):
+						Pos = TempHDD[j].find("rawValue")
+						TempHDD[j] = TempHDD[j][Pos + 10:]
+						Pos = TempHDD[j].find('"')
+						TempHDD[j] = TempHDD[j][:Pos]
+
+						if PrevAdaptecHDD[i][j - 1] != TempHDD[j]:
+							PrevAdaptecHDD[i][j - 1] = TempHDD[j]
+							self.ComQueue[MQTT].put([MQTT_PUBLISH, IDExternal + "temperature/adaptec" + str(i) + "/hdd" + str(j - 1), TempHDD[j]])
 
 			for i in range(0, len(Label)):
 				try:
